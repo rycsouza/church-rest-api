b1413fcb7357eda3bb6711886b1e3f790d6114b2
rycsouza <rychard.souza1111@gmail.com>
2024-07-20 17:38:56 -0400

    log pagamento

--- app/controllers/payments_controller.ts
+++ app/controllers/payments_controller.ts
@@ -4,6 +4,7 @@ import { VerificarPagamento } from '../helpers/payment/index.js'
 export default class PaymentsController {
   async handle({ request, response }: HttpContext) {
     const { payment } = request.all()
+    console.log('MEU P: ', payment)
     try {
       return await VerificarPagamento({ payment, response })
     } catch (error) {
