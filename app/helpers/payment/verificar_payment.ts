1204165fd257f4c54aa68b1bb26b04f70f0f4522
Rychard Souza <rychard.souza1111@gmail.com>
2024-08-12 10:31:07 -0400

    feat: continuando a implementação do comprovante de inscrição

--- app/helpers/payment/verificar_payment.ts
+++ app/helpers/payment/verificar_payment.ts
@@ -1,6 +1,9 @@
+import Church from '#models/church'
 import Constants from '#models/constants'
 import Inscricao from '#models/inscricao'
+import MailService from '#services/mail_service'
 import MercadoPagoService from '#services/mercado_pago_service'
+import { BuscarEventoIdHelper } from '../evento/index.js'
 
 export default async ({ payment, response }: any) => {
   try {
@@ -25,6 +28,24 @@ export default async ({ payment, response }: any) => {
 
     inscricao.situacaoId = Constants.Situacao.Concluido
     await inscricao.save()
+
+    //@ts-ignore
+    const inscricaoJSON = JSON.parse(inscricao.inscricaoJson)?.camposInscricao
+
+    if (!inscricaoJSON?.email)
+      return response.status(200).send(`Pagamento aprovado - Inscrição ${inscricao.id} concluída!`)
+
+    //@ts-ignore
+    // eslint-disable-next-line unicorn/no-await-expression-member
+    const churchConfig = JSON.parse((await Church.find(churchId))!.configJson)
+    const { evento } = await BuscarEventoIdHelper(inscricao.eventoId)
+
+    await MailService.send({
+      sender: churchConfig.email,
+      receiver: inscricaoJSON.email,
+      subject: `Comprovante de Inscrição - ${evento.nome}`,
+      htmlFile: 'comprovante_inscricao',
+    })
 
     return response.status(200).send(`Pagamento aprovado - Inscrição ${inscricao.id} concluída!`)
   } catch (error) {
--- app/services/mail_service.ts
+++ app/services/mail_service.ts
@@ -4,13 +4,15 @@ interface MailType {
   sender: string
   receiver: string
   subject: string
-  htmlPath: string
+  htmlFile: string
 }
 
+const htmlPath = '../../resources/mails'
+
 export default class MailService {
-  static async send({ sender, receiver, subject, htmlPath }: MailType) {
-    return await mail.send((message) => {
-      message.to(receiver).from(sender).subject(subject).htmlView(htmlPath)
+  static async send({ sender, receiver, subject, htmlFile }: MailType) {
+    return await mail.send((message: any) => {
+      message.to(receiver).from(sender).subject(subject).htmlView(`${htmlPath}/${htmlFile}.edge`)
     })
   }
 }
