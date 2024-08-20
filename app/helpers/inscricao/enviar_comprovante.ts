9689718f3b9995423f7239613566783fb8725363
rycsouza <rychard.souza1111@gmail.com>
2024-08-19 22:31:22 -0400

    feat: mudando a implementação do envio de emails e mensagens

--- app/helpers/inscricao/enviar_comprovante.ts
+++ app/helpers/inscricao/enviar_comprovante.ts
@@ -2,7 +2,11 @@
 import Inscricao from '#models/inscricao'
 import MailService from '#services/mail_service'
 import WhatsappService from '#services/whatsapp_service'
+import axios from 'axios'
 import { BuscarEventoIdHelper } from '../evento/index.js'
+import env from '#start/env'
+
+const URL_COMUNICACAO_API = env.get('URL_COMUNICACAO_API')
 
 export default async ({ inscricao }: { inscricao: Inscricao }) => {
   try {
@@ -21,49 +25,34 @@ export default async ({ inscricao }: { inscricao: Inscricao }) => {
     await evento.load('church')
 
     if (camposInscricao?.email) {
-      await MailService.send({
+      await axios.post(`${URL_COMUNICACAO_API}/mail`, {
+        email: camposInscricao.email,
         sender: { email: JSON.parse(evento.church.configJson)?.email, name: evento.church.nome },
-        receiver: camposInscricao.email,
-        subject: `${evento.nome} - Comprovante de Inscrição`,
-        htmlFile: 'comprovante_inscricao',
-        params: {
+        htmlParams: {
           evento,
           inscricao,
         },
+        templateTag: 'comprovante-inscricao',
       })
     }
 
     if (camposInscricao?.telefone) {
-      await WhatsappService.sendMediaMessageGenerateBasedHTMLTemplate({
-        data: {
-          id: inscricao.id,
-          telefone: camposInscricao?.telefone,
-          evento,
-          inscricao,
-        },
-        mensagem: 'Inscrição confirmada!✅ - Mostre esse comprovante no dia do evento!',
-        midiaFolder: 'comprovante_inscricao',
-        elementoHTML: '.container',
+      await axios.post(`${URL_COMUNICACAO_API}/whatsapp/media`, {
+        params: { id: inscricao.id, telefone: camposInscricao.telefone, evento, inscricao },
+        templateTag: 'comprovante-inscricao',
       })
     }
 
     if (camposResponsaveis && camposResponsaveis?.telefone !== camposInscricao?.telefone) {
-      await WhatsappService.sendMediaMessageGenerateBasedHTMLTemplate({
-        data: {
-          id: inscricao.id,
-          telefone: camposResponsaveis?.telefone,
-          evento,
-          inscricao,
-        },
-        mensagem: 'Inscrição confirmada!✅ - Mostre esse comprovante no dia do evento!',
-        midiaFolder: 'comprovante_inscricao',
-        elementoHTML: '.container',
+      await axios.post(`${URL_COMUNICACAO_API}/whatsapp/media`, {
+        params: { id: inscricao.id, telefone: camposResponsaveis.telefone, evento, inscricao },
+        templateTag: 'comprovante-inscricao',
       })
     }
 
     return true
   } catch (error) {
-    console.log(error)
+    console.log(error.response.data)
     return false
   }
 }
